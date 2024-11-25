a6e14f369d63638a6778b7a12967ed84d37cf87e
arahman225 <upworkabdurrahman@gmail.com>
2024-11-25 07:57:57 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,13 @@
+
+/**
+ * creating a post api on the server side such as: const newUser = req.body; newUser.id = users.length + 1;users.push(newUser); send: res.send(newUser)
+ * Client side -----------------------------------start
+ * Send data via post from client side such as below{
+ * Set fetch method inside fetch option (second parameter)
+ * Option will have three things: method ('POST)
+ * Option will have headers : 'Content-type: "application/json"
+ * body: JSON.stringify(data)}
+ * client side ------------------------------------end
+ * 
+ * Have to .use(express.json()) middleware on the server side. if not found search on the internet in this way "express req.body is undefined"
+ */
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PROT || 5000;
+
+
+
+const users = [
+    { id: 1, name: "sadik", email: "hadik1@gmail.com" },
+    { id: 2, name: "sadik", email: "hadik@gmail.com" },
+    { id: 3, name: "sadik", email: "sadik@gmail.com" }
+]
+
+app.get('/', (req, res) => {
+    res.send('Hi, wolrd')
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/users',(req, res) =>{
+    res.send(users)
+})
+
+app.post('/users',(req, res) =>{
+    console.log('Api hitting');
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser)
+})
+
+app.listen(port, () => {
+    console.log(`Running on the port ${port}`)
+})
